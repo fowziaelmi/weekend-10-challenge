78d4dbac98791352d065d623de9445abe5fc43fb
Faduma Elmi <fowziiae@gmail.com>
2021-02-14 09:53:45 -0600

    added input field and submit button

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use( bodyParser.json() );
+app.use(express.static('server/public'));
+
+// Start express
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log('up and running on port', PORT);
+});
