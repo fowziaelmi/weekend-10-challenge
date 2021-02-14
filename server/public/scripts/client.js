6483bedcd889df8a40623903be631fc168b64aa4
Faduma Elmi <fowziiae@gmail.com>
2021-02-14 16:22:49 -0600

    added checkbox

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,14 +1,208 @@
 $(document).ready(function () {
   console.log('in js');
   getTask();
-});
-function getTask() {
-  console.log('in get task');
-  $('#viewTasks').empty();
-  $.ajax({
-    url: '/tasks',
-    type: 'GET',
-  }).then(function (response) {
-    console.log('response', response); // end of ajax
+  $('#viewTasks').on('click', '#delete', onDelete);
+  $('#viewTasks').on('change', 'input[name=checkbox]', checkBox);
+  $('#createButton').on('click', function () {
+    console.log('task button has been clicked');
+    let newTask = {
+      task: $('#taskIn').val(),
+    };
+    console.log('the task input is :', newTask);
+    addTask(newTask);
   });
+
+  function getTask() {
+    console.log('in get task');
+    $('#viewTasks').empty();
+    $.ajax({
+      url: '/tasks',
+      method: 'GET',
+    })
+      .then(function (response) {
+        console.log('response', response);
+        drawTaskTable(response);
+        for (let i = 0; i < response.length; i++) {
+          $('#viewTasks').append(`
+        <div 
+          <input type="checkbox" name="checkbox" id="${
+            response[i].id
+          }"data-id="${response[i].id}" 
+          ${response[i].complete ? 'checked' : ''}>
+          <label class="taskss" for=${response[i].id}>
+          ${response[i].task_to_complete}</label>
+          <button class="${
+            response[i].complete ? '' : 'hidden'
+          }" id="delete" type="button" data-id="${
+            response[i].id
+          }">Delete</button>
+        </div>
+      `);
+        }
+      })
+      .catch(function (error) {
+        console.log('GET error', error);
+      });
+  }
+  // POST
+  function addTask(taskToDatabase) {
+    console.log('in addTask', taskToDatabase);
+    $.ajax({
+      url: '/tasks',
+      method: 'POST',
+      data: {
+        task_to_complete: $('#taskIn').val(),
+      },
+      // data = {task: 'taskinput'}
+    })
+      .then(function (response) {
+        $('#taskIn').val('');
+
+        getTask();
+      })
+      .catch(function (error) {
+        console.log('error on POST', error);
+      }); /*
+    $('#viewTasks').on('click', '.deleteButton', function () {
+      console.log('delete button was clicked');
+      let deleteId = $(this).parent().parent().data('id');
+      console.log(deleteId);
+      $.ajax({
+        method: 'DELETE',
+        url: '/tasks/' + deleteId,
+        success: function (response) {
+          getTask();
+        },
+      }); // end of ajax put delete request
+    }); // delete button listener
+  } */ //Delete
+  } //// function drawTaskTable( /// task ///) { // }, // end success // }); //end ajax //} // end of add task function // PUT
+
+  /*function appendTasks(task) {
+    for (let i = 0; i < task.length; i++) {
+      let taskToDisplay = task[i];
+      let taskRowToDisplay = $('<tr class = "taskRow"></tr>');
+      if (taskToDisplay.complete === false) {
+        $('#viewTasks').append(
+          '<td> <button class= "completeButton">Complete</button></td>'
+        );
+      } else {
+        taskRowToDisplay.addClass('completedRow');
+        taskRowToDisplay.append(``);
+      }
+      $('#viewTasks').append(taskRowToDisplay);
+    } // end of for loop
+  }
 }
+*/ /*$('#viewTasks').on('click', '#completeButton', function () {
+      console.log('complete button was clicked');
+      let completeId = $(this).parent().parent().data('id');
+      console.log(completeId);
+      $.ajax({
+        method: 'PUT',
+        url: '/tasks/complete'
+        success: function (response) {
+          getTask();
+        },
+      }); // end of ajax put complete request
+    }); // complete button listener
+/*
+    } */ function checkBox() {
+    let taskId = $(this).data('id');
+    console.log('checked id', taskId);
+
+    if ($(this).is(':checked')) {
+      completeTodo(taskId);
+    } else {
+      console.log('not checked');
+    }
+    getTask();
+  }
+
+  function completeTodo(taskId) {
+    console.log('in complete task', taskId);
+
+    $.ajax({
+      method: 'PUT',
+      url: `/tasks/complete/${taskId}`,
+
+      data: {
+        taskId,
+      },
+    })
+
+      .then(function (response) {
+        console.log('completed');
+        getTask();
+      })
+
+      .catch(function (error) {
+        console.log('PUT error', error);
+      });
+  }
+
+  function onDelete() {
+    console.log('delete button was clicked');
+    let deleteId = $(this).data('id');
+    console.log(deleteId);
+    $.ajax({
+      method: 'DELETE',
+      url: `/tasks/${deleteId}`,
+    })
+      .then(function (response) {
+        console.log('delete complete');
+        // Call getTodoList to update the DOM
+        getTask();
+      })
+      // Handle error
+      .catch(function (error) {
+        console.log('delete error', error);
+      }); // end of ajax put delete request
+  } // delete button listener }
+});
+/* function unCheck (taskId) {
+  console.log('in ', taskId);
+  // Set up PUT route
+  $.ajax({
+    method: 'PUT',
+    url: `/tasks/uncheck/${taskId}`,
+   
+    data: {
+      taskId,
+    },
+  })
+    
+    .then(function (response) {
+      console.log( 'success');
+    })
+    
+    .catch((error) => {
+      console.log('UNDO error', error);
+    });
+}
+*/
+/* function appendTasks(task) {
+  for (let i = 0; i < task.length; i++) {
+    let taskToDisplay = task[i];
+    //taskToDisplay.id, taskToDisplay.tasks_to_add, taskToDisplay.complete
+    let $taskRowToDisplay = $('<tr class = "taskRow"></tr>');
+    $taskRowToDisplay.data('id', taskToDisplay.id);
+    $taskRowToDisplay.append(
+      '<td class = "task">' + taskToDisplay.task_to_complete + '</td>'
+    );
+
+    if (taskToDisplay.complete === true) {
+      $taskRowToDisplay.append(
+        '<td><button class= "completeButton">Complete</button></td>'
+      );
+    } else {
+      $taskRowToDisplay.addClass('completedRow');
+      $taskRowToDisplay.append(
+        '<td><button class = "deleteButton">Delete</button></td>'
+      );
+    }
+    $('#viewTasks').append($taskRowToDisplay);
+  } 
+
+} 
+*/
--- server/routes/task_router.js
+++ server/routes/task_router.js
@@ -21,4 +21,59 @@ taskRouter.get('/', (req, res) => {
       res.sendStatus(500);
     });
 });
+
+//POST endpoint
+taskRouter.post('/', (req, res) => {
+  console.log('req.body', req.body);
+  //query the database
+  let sqlText = `INSERT INTO "tasks" ("task_to_complete") VALUES($1)`;
+  let sqlArgs = [req.body.task_to_complete];
+
+  pool
+    .query(sqlText, sqlArgs)
+    .then(function (dbRes) {
+      res.sendStatus(200);
+    })
+    .catch(function (error) {
+      console.log('POST error', error);
+    });
+});
+
+//PUT
+
+taskRouter.put('/complete/:id', (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+
+  let taskId = req.params.id;
+  // let newValue = req.body.read;
+
+  let sqlText = `UPDATE "tasks" SET "complete" = 'TRUE' WHERE "id"=$1`;
+
+  pool
+    .query(sqlText, [taskId])
+    .then((dbRes) => {
+      console.log('dbRes is ', dbRes);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+taskRouter.delete('/:id', function (req, res) {
+  let deleteId = req.params.id;
+  let sqlText = ` DELETE FROM "tasks" WHERE "id"=$1 `;
+
+  pool
+    .query(sqlText, [deleteId])
+    .then((dbRes) => {
+      console.log('task deleted');
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log('error on delete', error);
+    });
+});
+
 module.exports = taskRouter;
